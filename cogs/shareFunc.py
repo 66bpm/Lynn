2490891bfff0661421043578d3b6807a1fca5738
66bpm <miko.reddust@gmail.com>
2020-11-24 02:03:10 +0700

    Untab extra indent

--- cogs/shareFunc.py
+++ cogs/shareFunc.py
@@ -84,48 +84,48 @@ async def SendFieldOnlyEmbed(cog, ctx, name, value):
     await ctx.send(embed = embedMessage)
 
 async def Notify(cog, hentaiRow, channelRows):
-        messages = []
-        embedMessage = await EmbedFormatter(cog, hentaiRow)
-        if (hentaiRow[5] != None): 
-            tags = set(hentaiRow[5])
+    messages = []
+    embedMessage = await EmbedFormatter(cog, hentaiRow)
+    if (hentaiRow[5] != None): 
+        tags = set(hentaiRow[5])
+    else:
+        tags = None
+    if (hentaiRow[8] != None):
+        hlan = set(hentaiRow[8])
+    else:
+        hlan = None
+    for channelRow in channelRows:
+        channel = cog.bot.get_channel(channelRow[0])
+        if (channelRow[1] != None):
+            incl = set(channelRow[1])
         else:
-            tags = None
-        if (hentaiRow[8] != None):
-            hlan = set(hentaiRow[8])
+            incl = None
+        if (channelRow[2] != None):
+            excl = set(channelRow[2])
         else:
-            hlan = None
-        for channelRow in channelRows:
-            channel = cog.bot.get_channel(channelRow[0])
-            if (channelRow[1] != None):
-                incl = set(channelRow[1])
-            else:
-                incl = None
-            if (channelRow[2] != None):
-                excl = set(channelRow[2])
-            else:
-                excl = None
-            if (channelRow[3] != None):
-                lan = set(channelRow[3])
-            else:
-                lan = None
-            if (channel!= None):
-                if (tags == None and incl == None):
-                    message = await channel.send(embed = embedMessage)
-                    messages.append(message)
-                elif (tags != None):
-                    tagFlag = tags
-                    if (incl != None):
-                        tagFlag = tags.intersection(incl)
-                    if (len(tagFlag) > 0):
-                        if (excl != None):
-                            tagFlag = tags.intersection(excl)
-                        else:
-                            tagFlag = set()
-                        if (len(tagFlag) < 1):
-                            lanFlag = hlan
-                            if (lan != None):
-                                lanFlag = hlan.intersection(lan)
-                            if (len(lanFlag) > 0):
-                                message = await channel.send(embed = embedMessage)
-                                messages.append(message)
-        return messages
+            excl = None
+        if (channelRow[3] != None):
+            lan = set(channelRow[3])
+        else:
+            lan = None
+        if (channel!= None):
+            if (tags == None and incl == None):
+                message = await channel.send(embed = embedMessage)
+                messages.append(message)
+            elif (tags != None):
+                tagFlag = tags
+                if (incl != None):
+                    tagFlag = tags.intersection(incl)
+                if (len(tagFlag) > 0):
+                    if (excl != None):
+                        tagFlag = tags.intersection(excl)
+                    else:
+                        tagFlag = set()
+                    if (len(tagFlag) < 1):
+                        lanFlag = hlan
+                        if (lan != None):
+                            lanFlag = hlan.intersection(lan)
+                        if (len(lanFlag) > 0):
+                            message = await channel.send(embed = embedMessage)
+                            messages.append(message)
+    return messages
